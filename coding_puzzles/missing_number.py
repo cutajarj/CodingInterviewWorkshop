42fe9d7a8872d7ced3eae63e7a1e80761f3857e6
cutajarj2 <james.cutajar@paddypowerbetfair.com>
2022-11-17 18:19:55 +0000

    Adding missing numbers solution

--- coding_puzzles/missing_number.py
+++ coding_puzzles/missing_number.py
@@ -17,6 +17,7 @@
 # Some more examples:
 # find_missing([2,4,7,3,8,5,1]) should return 6
 # find_missing([2,4,6,7,3,8,5]) should return 1
+# find_missing([2,1,4]) should return 3
 # find_missing([2,1,3]) should return 4
 # find_missing([1]) should return 2
 # find_missing([]) should return 1
