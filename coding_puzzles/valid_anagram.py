0285a68bbdcc7ff8aa6d51cdfbdda87d8b930f27
cutajarj2 <james.cutajar@paddypowerbetfair.com>
2022-11-29 12:05:34 +0000

    Adding anagram

---
+++ coding_puzzles/valid_anagram.py
@@ -0,0 +1,22 @@
+# Complete the body of the function shown below
+# The input of the function accepts two arguments
+#   s: A string of length 0 or more characters
+#   t: A string of length 0 or more characters
+#
+# The function should return true iff the strings are anagrams of each other
+# Characters are case sensitive.
+# Assume that the string is not UTF.
+#
+# Examples:
+# s = "binary", t = "brainy" result = true
+# s = "rat", t = "car" result = false
+# s = "BAT", t = "tab" result = false (different case)
+# s = "rail safety", t = "fairy tales" result = true
+# s = "the eyes", t = "they see" result = true
+# s = "debit card", t = "bad credit" result = true
+# s = "action man", t = "cannot aim" result = true
+# s = "", t = "" result = true
+# s = "batt", t = "tab" result = false
+def is_anagram(s, t):
+    return False
+
---
+++ tests/valid_anagram_test.py
@@ -0,0 +1,32 @@
+import unittest
+from coding_puzzles import valid_anagram
+
+
+class TestValidAnagram(unittest.TestCase):
+    def test_brainy(self):
+        self.assertTrue(valid_anagram.is_anagram("binary", "brainy"))
+
+    def test_car(self):
+        self.assertFalse(valid_anagram.is_anagram("rat", "car"))
+
+    def test_tab(self):
+        self.assertFalse(valid_anagram.is_anagram("BAT", "tab"))
+
+    def test_fairy_tales(self):
+        self.assertTrue(valid_anagram.is_anagram("rail safety", "fairy tales"))
+
+    def test_they_see(self):
+        self.assertTrue(valid_anagram.is_anagram("the eyes", "they see"))
+
+    def test_bad_credit(self):
+        self.assertTrue(valid_anagram.is_anagram("debit card", "bad credit"))
+
+    def test_cannot_aim(self):
+        self.assertTrue(valid_anagram.is_anagram("action man", "cannot aim"))
+
+    def test_empty(self):
+        self.assertTrue(valid_anagram.is_anagram("", ""))
+
+    def test_bad_length(self):
+        self.assertTrue(valid_anagram.is_anagram("batt", "tab"))
+
