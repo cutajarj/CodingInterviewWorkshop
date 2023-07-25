b4e5e42b4bc70d63eb2d36d3578fe4591af6c92d
cutajarj2 <james.cutajar@paddypowerbetfair.com>
2023-07-25 19:47:53 +0100

    Adding new problem

--- coding_puzzles/first_non_repeating.py
+++ coding_puzzles/first_non_repeating.py
@@ -1,3 +1,11 @@
 
 def find_first_non_repeating(text):
-    return ""
+    table = [0] * 256
+    for c in text:
+        table[ord(c)] += 1
+    result = ""
+    for c in text:
+        if table[ord(c)] == 1:
+            result = c
+            break
+    return result
