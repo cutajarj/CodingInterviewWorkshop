498a036b616423dc86198ae154a5f71f4a3190e5
cutajarj <james.cutajar@gmail.com>
2024-02-20 17:22:12 +0000

    Adding new problem

--- coding_puzzles/sudoku_generater.py
+++ coding_puzzles/sudoku_generater.py
@@ -2,7 +2,12 @@ import random
 
 sudoku = [[0 for i in range(9)] for j in range(9)]
 
-
+# Implement this function to populate the 2d array with a random valid sudoku
+# Everytime you run this program a different sudoku should be printed
+# A sudoku is valid if:
+#   each separate row and column does not contain duplicate numbers
+#   each of the 9, 3x3 boxes don't contain duplicate numbers
+#   only numbers from 1 to 9 are allowed in each cell
 def calculate_cell(row_number, col_number):
     return False
 
