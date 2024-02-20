6d53ea285bb06899ff21ef3a2f746951daf11829
cutajarj <james.cutajar@gmail.com>
2024-02-20 17:14:33 +0000

    Adding new problem

--- coding_puzzles/sudoku_generater.py
+++ coding_puzzles/sudoku_generater.py
@@ -4,6 +4,20 @@ sudoku = [[0 for i in range(9)] for j in range(9)]
 
 
 def calculate_cell(row_number, col_number):
+    if row_number > 8:
+        return True
+    rand_numbers = list(range(1, 10))
+    random.shuffle(rand_numbers)
+    for n in rand_numbers:
+        if is_valid(n, row_number, col_number):
+            sudoku[row_number][col_number] = n
+            r, c = row_number, col_number + 1
+            if c == 9:
+                r, c = row_number + 1, 0
+            if calculate_cell(r, c):
+                return True
+            else:
+                sudoku[row_number][col_number] = 0
     return False
 
 
@@ -27,4 +41,3 @@ def print_sudoku():
 
 calculate_cell(0, 0)
 print_sudoku()
-
