50548d382fd1ab82827f27d15864651ec2c86eae
cutajarj <james.cutajar@gmail.com>
2024-02-27 18:14:36 +0000

    Fixing solution

--- coding_puzzles/first_non_repeating.py
+++ coding_puzzles/first_non_repeating.py
@@ -1,11 +1,16 @@
-
 def find_first_non_repeating(text):
-    table = [0] * 256
-    for c in text:
-        table[ord(c)] += 1
-    result = ""
-    for c in text:
-        if table[ord(c)] == 1:
-            result = c
-            break
+    table = [(0, 0)] * 256
+    for i in range(len(text)):
+        c = text[i]
+        (count, position) = table[ord(c)]
+        if count == 0:
+            table[ord(c)] = (count + 1, i)
+        else:
+            table[ord(c)] = (count + 1, position)
+    result, lowest_position = "", len(text)
+    for i in range(len(table)):
+        count, position = table[i]
+        if position < lowest_position and count == 1:
+            lowest_position = position
+            result = chr(i)
     return result
