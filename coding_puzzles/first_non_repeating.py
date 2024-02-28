6ad1b3f4e665e13299c5474cf263149fca5119ca
cutajarj <james.cutajar@gmail.com>
2024-02-28 11:01:38 +0000

    Adding description

--- coding_puzzles/first_non_repeating.py
+++ coding_puzzles/first_non_repeating.py
@@ -1,3 +1,15 @@
+# A string might contain characters that repeat. In this coding puzzle the solution
+# needs to return the first character that doesn't repeat.
+#
+# Given an input string, return the first character from the left, that appears only once
+# If all the characters in the input repeat, the function should return an empty string
+# The function should be case-sensitive.
+# Assume only english characters and normal punctuation is used.
+# Examples:
+# "seashore" should return "a" because "s" and "e" repeat
+# "systematisation systems" should return "o"
+# "fudgefudge" should return "" since all the characters repeat
+# "fudGefudge" should return "G"
 
 def find_first_non_repeating(text):
     return ""
