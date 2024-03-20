7bbba8d4f69a2dbd4283af80ceb668981ecae172
cutajarj <james.cutajar@gmail.com>
2024-03-20 11:45:30 +0000

    Adding solution to soduku checker

--- coding_puzzles/sudoku_checker.py
+++ coding_puzzles/sudoku_checker.py
@@ -9,6 +9,21 @@
 # each element in the array is in the range [1,9]
 
 def is_valid(sudoku):
+    numbers_rows = [1] * 9
+    numbers_cols = [1] * 9
+    numbers_box = [1] * 9
+    for i in range(9):
+        for j in range(9):
+            n_row = sudoku[j][i]
+            numbers_rows[n_row - 1] = 0
+            n_col = sudoku[i][j]
+            numbers_cols[n_col - 1] = 0
+            r, c = (i % 3 * 3) + j % 3, (i // 3 * 3) + j // 3
+            n_box = sudoku[r][c]
+            numbers_box[n_box] = 0
+        if sum(numbers_rows) + sum(numbers_cols) + sum(numbers_box) != 0:
+            return False
+
     return True
 
 
